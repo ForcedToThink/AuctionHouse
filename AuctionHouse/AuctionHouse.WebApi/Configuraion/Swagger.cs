c8e3d7b6666b28a8c649ff7126207a774b2e780a
Dawid Lipski <dawidlipski89@gmail.com>
2018-07-31 03:11:15 +0200

    add webApi project with simple swagger

---
+++ AuctionHouse/AuctionHouse.WebApi/Configuraion/Swagger.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionHouse.WebApi.Configuraion
+{
+    /// <summary>
+    ///     Contains extension methods for Swagger configuration.
+    /// </summary>
+    public static class Swagger
+    {
+        /// <summary>
+        ///     Adds swagger to service collection.
+        /// </summary>
+        /// <param name="services">The service collection</param>
+        /// <returns></returns>
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Version = "v1",
+                    Title = "Auction House API",
+                    Description = "Auction House API"
+                });
+                c.IncludeXmlComments(GetXmlCommentsPath());
+            });
+
+            return services;
+        }
+
+        /// <summary>
+        ///     Uses swagger in application builder.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Auction House v1"); });
+            return app;
+        }
+
+        private static string GetXmlCommentsPath()
+        {
+            var basePath = System.AppContext.BaseDirectory;
+            var fileName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name + ".xml";
+            return Path.Combine(basePath, fileName);
+        }
+    }
+}
